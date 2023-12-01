1a6c4bb88131437a567e8b4c6a65525213d67f91
Fred Sheppard <frederickjssheppard@gmail.com>
2023-12-01 16:55:43 +0000

    Move Grid.findNearestTaxi to Region

--- src/main/java/com/ise/taxiapp/nav/Grid.java
+++ src/main/java/com/ise/taxiapp/nav/Grid.java
@@ -1,10 +1,5 @@
 package com.ise.taxiapp.nav;
 
-import com.ise.taxiapp.entities.Fare;
-import com.ise.taxiapp.entities.Taxi;
-
-import java.util.Comparator;
-
 public class Grid extends Region {
 
     private final int width;
@@ -21,23 +16,6 @@ public class Grid extends Region {
 
     public int getHeight() {
         return height;
-    }
-
-    /**
-     * Finds the nearest taxi to the given location for the given fare.
-     * May return null if no taxis are available.
-     *
-     * @param location The location where the taxi must travel to
-     * @param fare     The fare to filter by when finding a taxi
-     * @return The nearest taxi to the location, or null if no taxi is available
-     */
-    @Override
-    public Taxi findNearestTaxi(Location location, Fare fare) {
-        return taxiList
-                .stream()
-                .filter(taxi -> taxi.getFare().equals(fare))
-                .min(Comparator.comparing(taxi -> taxi.getLocation().distanceTo(location)))
-                .orElse(null);
     }
 
     @Override
--- src/main/java/com/ise/taxiapp/nav/Region.java
+++ src/main/java/com/ise/taxiapp/nav/Region.java
@@ -3,10 +3,11 @@ package com.ise.taxiapp.nav;
 import com.ise.taxiapp.entities.Fare;
 import com.ise.taxiapp.entities.Taxi;
 
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 
-public abstract class Region {
+public class Region {
     protected List<Taxi> taxiList;
 
     Region() {
@@ -21,5 +22,19 @@ public abstract class Region {
         taxiList.remove(taxi);
     }
 
-    public abstract Taxi findNearestTaxi(Location other, Fare fare);
+    /**
+     * Finds the nearest taxi to the given location for the given fare.
+     * May return null if no taxis are available.
+     *
+     * @param location The location where the taxi must travel to
+     * @param fare     The fare to filter by when finding a taxi
+     * @return The nearest taxi to the location, or null if no taxi is available
+     */
+    public Taxi findNearestTaxi(Location location, Fare fare) {
+        return taxiList
+                .stream()
+                .filter(taxi -> taxi.getFare().equals(fare))
+                .min(Comparator.comparing(taxi -> taxi.getLocation().distanceTo(location)))
+                .orElse(null);
+    }
 }
