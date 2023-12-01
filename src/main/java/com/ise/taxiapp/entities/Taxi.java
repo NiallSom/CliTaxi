03999b84c8f5f3ae953d9dd7172c11dd8924938e
Fred Sheppard <frederickjssheppard@gmail.com>
2023-12-01 16:51:27 +0000

    Create getter and setter for Fare

--- src/main/java/com/ise/taxiapp/entities/Taxi.java
+++ src/main/java/com/ise/taxiapp/entities/Taxi.java
@@ -3,35 +3,47 @@ package com.ise.taxiapp.entities;
 import com.ise.taxiapp.UI;
 import com.ise.taxiapp.nav.Location;
 
-public class Taxi{
+public class Taxi {
     private String reg;
     private Fare fare;
     private Location currentLocation;
     private Driver driver;
     private User user;
     private UI ui;
-
-    public Taxi(String reg, Driver driver,Fare fare){
+    public Taxi(String reg, Driver driver, Fare fare) {
         this.reg = reg;
         this.driver = driver;
         this.fare = fare;
     }
-    public void driveTo(Location location){
+
+    public Fare getFare() {
+        return fare;
+    }
+
+    public void setFare(Fare fare) {
+        this.fare = fare;
+    }
+
+    public void driveTo(Location location) {
         ui.informOnRoute();
     }
-    public void driveTo(Location location, Fare fare){
+
+    public void driveTo(Location location, Fare fare) {
         return;//implement 2nd drive to method
     }
-    public void pickup(User user){
+
+    public void pickup(User user) {
         return; // implement pickup method
     }
-    public void task(User user, Location destination){
+
+    public void task(User user, Location destination) {
         driveTo(user.currentLocation);
         pickup(user);
         driveTo(destination, this.fare);
         user.charge(this.fare, user.currentLocation.distanceTo(destination));
     }
-    public Location getLocation(){
+
+    public Location getLocation() {
         return currentLocation;
     }
 
