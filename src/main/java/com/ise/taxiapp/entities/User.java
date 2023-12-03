a4cc05c7043e687d94526caf41fe2aa0f46c4c74
Fred Sheppard <frederickjssheppard@gmail.com>
2023-12-03 00:34:00 +0000

    Remove callTaxi from User
    - Add getters and setters

--- src/main/java/com/ise/taxiapp/entities/User.java
+++ src/main/java/com/ise/taxiapp/entities/User.java
@@ -2,24 +2,37 @@ package com.ise.taxiapp.entities;
 
 import com.ise.taxiapp.nav.Location;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class User {
-    String username;
-    List<Ride> rideHistory;
-    Location currentLocation;
-    double balance;
+    private final String username;
+    private final List<Ride> rideHistory;
+    private Location currentLocation;
+    private double balance;
 
     public User(String username) {
         this.username = username;
+        rideHistory = new LinkedList<>();
     }
 
-    public void callTaxi(Location destination, Fare fare) {
-        Taxi taxi = destination.getRegion().findNearestTaxi(currentLocation, fare);
-        taxi.task(this, destination);
+    public Location getCurrentLocation() {
+        return currentLocation;
     }
 
-    public void charge(Fare fare, double distance) {
-        this.balance -= fare.calculateFare(distance);
+    public void setCurrentLocation(Location currentLocation) {
+        this.currentLocation = currentLocation;
+    }
+
+    public void charge(double charge) {
+        balance -= charge;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public List<Ride> getRideHistory() {
+        return rideHistory;
     }
 }
