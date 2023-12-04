275b2df98fdc4fbed551bb06f96497d8142c15fb
Niall <niallsomers205@gmail.com>
2023-12-04 16:07:34 +0000

    Updated to doubly linked list, should be fully working now

--- src/main/java/com/ise/taxiapp/dataStructures/LinkedList.java
+++ src/main/java/com/ise/taxiapp/dataStructures/LinkedList.java
@@ -1,57 +1,80 @@
 package com.ise.taxiapp.dataStructures;
 
 public class LinkedList<T> {
-
     private ListNode<T> head;
     private ListNode<T> current;
-    private static class ListNode<T>{
-        private T data;
+    private class ListNode<T> {
+        private final T data;
         private ListNode<T> next;
-        // need to implement doubly linked list
-        public ListNode(T data){
+        private ListNode<T> previous;
+        public ListNode(T data) {
             this.data = data;
             this.next = null;
+            this.previous = null;
         }
     }
-
-    public boolean isEmpty() {
-        return head == null;
-    }
-
-    public ListNode<T> retrieve(T element) {
-        if (isEmpty()){
-            System.out.println("List is empty, cannot retrieve element: "+element); // not sure how to string format with generics
-            return null;
-        }
-        ListNode<T> current = head;
-        while (current != null) {
-            if (current.data != null && current.data.equals(element)) {
-                return current;
-            }
-            current = current.next;
-        }
-        return null;
-    }
-
-    public void update(T element, T data) {
-        retrieve(element).data = data;
-    }
-
-    public void insert(T element) {
-        if (isEmpty()) {
-            System.out.println("List is empty, element will be inserted at location 0");
-            head = new ListNode<>(element);
-            current = head;
+    public void getNext(){
+        if (this.current.next == null) {
+            System.out.println("There is no next node");
             return;
         }
+        this.current = this.current.next;
+    }
+    public void getPrevious(){
+        if (this.current.previous == null) {
+            System.out.println("There is no previous node");
+            return;
+        }
+        this.current = this.current.previous;
+    }
+    public boolean isEmpty(){
+        return head == null;
+    }
+    public T retrieve() {
+        return this.current.data;
+    }
+    public void insert(T data){
+        ListNode<T> newNode = new ListNode<T>(data);
+        if (isEmpty()) {
+            System.out.println("List is empty, creating a new head");
+            this.current = head = newNode;
+            return;
+        }
+        newNode.next = this.current.next;
+        newNode.previous = this.current;
+        if (this.current.next != null) {
+            this.current.next.previous = newNode;
+        }
+        this.current.next = newNode;
+        this.current = newNode;
+
+    }
+    public void printList(){
         ListNode<T> temp = head;
-        ListNode<T> newNode = new ListNode<T>(element);
-        while (temp.next != null) {
+        while (temp != null){
+            System.out.print(temp.data+" -> ");
             temp = temp.next;
         }
-        temp.next = newNode;
-        current = newNode;
+        System.out.println();
+    }
+    public void remove() {
+        if (isEmpty()) {
+            System.out.println("List is empty cannot remove item");
+            return;
+        }
+        if (this.current == head){
+            if (this.current.next != null){
+                head = this.current.next;
+                return;
+            }
+            head = null;
+        }
+
+        if (this.current.next != null) {
+            this.current.next.previous = this.current.previous;
+            this.current.previous.next = this.current.next;
+            this.current = this.current.next;
+        }
+
     }
 }
-
-
