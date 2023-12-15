738b8a1bd085e5e8ec7e8b0e6b53496a0268c9c5
Fred Sheppard <frederickjssheppard@gmail.com>
2023-12-15 17:05:34 +0000

    Create Locatable interface

--- src/main/java/com/ise/taxiapp/entities/User.java
+++ src/main/java/com/ise/taxiapp/entities/User.java
@@ -2,26 +2,16 @@ package com.ise.taxiapp.entities;
 
 import com.ise.taxiapp.nav.Location;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.Objects;
 
-public class User {
+public class User implements Locatable {
     private final String username;
-    private final List<Ride> rideHistory;
     private Location currentLocation;
     private double balance;
 
     public User(String username) {
         this.username = username;
-        rideHistory = new LinkedList<>();
-    }
-
-    public Location getCurrentLocation() {
-        return currentLocation;
-    }
-
-    public void setCurrentLocation(Location currentLocation) {
-        this.currentLocation = currentLocation;
+        this.balance = 20.0;
     }
 
     public void charge(double charge) {
@@ -32,11 +22,25 @@ public class User {
         return username;
     }
 
-    public List<Ride> getRideHistory() {
-        return rideHistory;
-    }
-
     public double getBalance() {
         return balance;
+    }
+
+    @Override
+    public Location getLocation() {
+        return currentLocation;
+    }
+
+    @Override
+    public void setLocation(Location location) {
+        this.currentLocation = location;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (other == null || getClass() != other.getClass()) return false;
+        User user = (User) other;
+        return Objects.equals(username, user.username);
     }
 }
