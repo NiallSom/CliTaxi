2949b7219c6959b41bbce1ecf7c716655f75a965
Fred Sheppard <frederickjssheppard@gmail.com>
2023-12-15 17:07:44 +0000

    Create AsciiColours for printing colours to the terminal

---
+++ src/main/java/com/ise/taxiapp/cli/AsciiColours.java
@@ -0,0 +1,9 @@
+package com.ise.taxiapp.cli;
+
+public class AsciiColours {
+    public final static String RED = "\u001B[31m";
+    public final static String GREEN = "\u001B[32m";
+    public final static String YELLOW = "\u001B[33m";
+    public final static String CYAN = "\u001B[36m";
+    public final static String RESET = "\033[0m";
+}
