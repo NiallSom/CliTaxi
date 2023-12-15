6d1b0a3a4613958ac08cdf27791a0e7bc47b4f33
Fred Sheppard <frederickjssheppard@gmail.com>
2023-12-15 17:08:53 +0000

    Update Point to contain a list of Locatables

--- src/test/java/NavTest.java
+++ src/test/java/NavTest.java
@@ -1,3 +1,4 @@
+import com.ise.taxiapp.entities.User;
 import com.ise.taxiapp.nav.Grid;
 import com.ise.taxiapp.nav.Point;
 import org.junit.jupiter.api.Assertions;
@@ -17,18 +18,18 @@ public class NavTest {
     void adjacent() {
         Point origin = grid.get(5, 5);
         Point goal = grid.get(5, 6);
-        goal.getObjects().add("Goal!");
-        Object result = grid.findNearest(origin, "Goal!"::equals, 1);
-        Assertions.assertEquals("Goal!", result);
+        goal.getObjects().add(new User("Name"));
+        Object result = grid.findNearest(origin, new User("Name")::equals, 1);
+        Assertions.assertEquals(new User("Name"), result);
     }
 
     @Test
     void twoAway() {
         Point origin = grid.get(3, 2);
         Point goal = grid.get(5, 2);
-        goal.getObjects().add("Goal!");
-        Object result = grid.findNearest(origin, "Goal!"::equals, 2);
-        Assertions.assertEquals("Goal!", result);
+        goal.getObjects().add(new User("Name"));
+        Object result = grid.findNearest(origin, new User("Name")::equals, 2);
+        Assertions.assertEquals(new User("Name"), result);
     }
 
 
@@ -36,23 +37,23 @@ public class NavTest {
     void diagonal() {
         Point origin = grid.get(3, 3);
         Point goal = grid.get(4, 4);
-        goal.getObjects().add("Goal!");
-        Object result = grid.findNearest(origin, "Goal!"::equals, 2);
-        Assertions.assertEquals("Goal!", result);
+        goal.getObjects().add(new User("Name"));
+        Object result = grid.findNearest(origin, new User("Name")::equals, 2);
+        Assertions.assertEquals(new User("Name"), result);
     }
 
     @Test
     void smallRadius() {
         Point origin = grid.get(3, 3);
         Point goal = grid.get(3, 5);
-        goal.getObjects().add("Goal!");
-        Object result = grid.findNearest(origin, "Goal!"::equals, 1);
+        goal.getObjects().add(new User("Name"));
+        Object result = grid.findNearest(origin, new User("Name")::equals, 1);
         Assertions.assertNull(result);
 
-        Object result2 = grid.findNearest(origin, "Goal!"::equals, 2);
+        Object result2 = grid.findNearest(origin, new User("Name")::equals, 2);
         Assertions.assertNotNull(result2);
 
-        Object result3 = grid.findNearest(origin, "Goal!"::equals, 3);
+        Object result3 = grid.findNearest(origin, new User("Name")::equals, 3);
         Assertions.assertNotNull(result3);
     }
 
@@ -61,9 +62,9 @@ public class NavTest {
     void edgeOfGrid() {
         Point origin = grid.get(0, 0);
         Point goal = grid.get(0, 1);
-        goal.getObjects().add("Goal!");
-        Object result = grid.findNearest(origin, "Goal!"::equals, 1);
-        Assertions.assertEquals("Goal!", result);
+        goal.getObjects().add(new User("Name"));
+        Object result = grid.findNearest(origin, new User("Name")::equals, 1);
+        Assertions.assertEquals(new User("Name"), result);
     }
 
     @Test
