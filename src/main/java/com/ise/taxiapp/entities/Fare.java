5eb1c77c9dd8f20500f682fef6a89c7680eebbe9
Fred Sheppard <frederickjssheppard@gmail.com>
2023-12-15 17:09:16 +0000

    Update Fare to use colours

--- src/main/java/com/ise/taxiapp/entities/Fare.java
+++ src/main/java/com/ise/taxiapp/entities/Fare.java
@@ -1,20 +1,29 @@
 package com.ise.taxiapp.entities;
 
+import static com.ise.taxiapp.cli.AsciiColours.*;
+
 public enum Fare {
-    STANDARD_FARE(5, 4.5),
-    EXTRA_LARGE_FARE(6, 6),
-    EXPRESS_FARE(10, 4);
+    STANDARD_FARE(5, 4.5, CYAN),
+    EXTRA_LARGE_FARE(6, 6, YELLOW),
+    EXPRESS_FARE(10, 4, GREEN);
+
 
     final private double initial;
     final private double meter;
+    final private String colour;
 
-    Fare(double initial, double meter) {
+    Fare(double initial, double meter, String colour) {
         this.initial = initial;
         this.meter = meter;
+        this.colour = colour;
     }
 
     public double calculateCharge(double distance) {
         return (distance * meter) + initial;
+    }
+
+    public String getColour() {
+        return colour;
     }
 
 }
